91aaa8f5ecbd13407769a2af241a827ef584fc54
Esmyrna <103766811+Esmyrna@users.noreply.github.com>
2025-05-25 19:12:45 -0300

    Update UserRepositoryImpl.java

--- src/main/java/com/crud/crudusuarios/infra/repositories/UserRepositoryImpl.java
+++ src/main/java/com/crud/crudusuarios/infra/repositories/UserRepositoryImpl.java
@@ -3,5 +3,6 @@ package com.crud.crudusuarios.infra.repositories;
 import com.crud.crudusuarios.domain.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface UserRepositoryImpl extends JpaRepository<User, Long>, UserRepository {
 }
